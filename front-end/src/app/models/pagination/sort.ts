b1660ea855102870670085d74b5d6b2c704c5882
NgocThach <hnt1693@gmail.com>
2020-09-06 18:36:47 +0700

    thach merge

--- front-end/src/app/models/pagination/sort.ts
+++ front-end/src/app/models/pagination/sort.ts
@@ -2,3 +2,14 @@ export class Sort {
   sorted: boolean;
   unsorted: boolean;
 }
+
+export class MySort {
+  active: string;
+  direction: string;
+
+
+  constructor(active: string, direction: string) {
+    this.active = active;
+    this.direction = direction;
+  }
+}
